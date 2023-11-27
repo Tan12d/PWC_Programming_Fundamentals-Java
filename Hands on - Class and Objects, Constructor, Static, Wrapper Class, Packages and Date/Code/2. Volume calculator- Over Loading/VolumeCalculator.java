138cf6c4a2e7d5736dc68c8cb674ac92688c0011
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-11-27 12:17:42 +0530

    Create VolumeCalculator.java

---
+++ Hands on - Class and Objects, Constructor, Static, Wrapper Class, Packages and Date/Code/2. Volume calculator- Over Loading/VolumeCalculator.java
@@ -0,0 +1,15 @@
+public class VolumeCalculator {
+ // Fill the code
+
+ public double calculateVolume(double radius, double height)
+ {
+ double volume= 3.14*radius*radius*height;
+ return volume;
+ }
+
+ public double calculateVolume(int length, int breadth, int height)
+ {
+ double volume= length*breadth*height;
+ return volume;
+ }
+}
